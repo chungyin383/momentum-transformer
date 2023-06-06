afa18b7c578624677b12f054562e3b5077849047
chungyin383 <34208851+chungyin383@users.noreply.github.com>
2023-06-06 19:41:41 +0800

    add download_coingecko_data.py

---
+++ data/download_coingecko_data.py
@@ -0,0 +1,41 @@
+import requests
+from datetime import datetime
+import pandas as pd
+import os
+from settings.default import CRYPTO_TICKERS
+
+MARKET_CHART_API = "https://api.coingecko.com/api/v3/coins/{coin}/market_chart"
+MARKET_CHART_PARAMS = {
+    "vs_currency": "usd",
+    "days": "max",
+}
+
+
+def main():
+
+    if not os.path.exists(os.path.join("data", "coingecko")):
+        os.mkdir(os.path.join("data", "coingecko"))
+
+    # Fetch data for each ticker and save it as a separate CSV file
+    for ticker in CRYPTO_TICKERS:
+        print(ticker)
+        try:
+            response = requests.get(MARKET_CHART_API.format(coin=ticker), params=MARKET_CHART_PARAMS)
+            prices = response.json()["prices"]
+            volumes = response.json()["total_volumes"]
+            data = [(
+                datetime.fromtimestamp(price[0]/1000).strftime("%Y-%m-%d"),
+                price[1],
+                volumes[i][1]
+            ) for i, price in enumerate(prices)]
+            df = pd.DataFrame(data, columns=["Date", "Settle", "Volume"])
+            df.set_index("Date", inplace=True)
+            df.to_csv(
+                os.path.join("data", "coingecko", f"{ticker}.csv")
+            )
+        except Exception as ex:
+            print(ex)
+        
+
+if __name__ == "__main__":
+    main()
