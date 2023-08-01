ea9862d4863dffabb5a3bcb30aa53cce8a652642
chungyin383 <34208851+chungyin383@users.noreply.github.com>
2023-08-01 13:24:25 +0800

    Update plot_returns_graph.py

--- examples/plot_returns_graph.py
+++ examples/plot_returns_graph.py
@@ -10,7 +10,15 @@ EXPERIMENTS = [
     'experiment_quandl_100assets_tft_cpnone_len252_notime_div_v1',
     'experiment_quandl_100assets_tft_cp12621_len252_notime_div_v4',
     'experiment_quandl_100assets_tft_cp12621_len252_notime_div_rsi_kd_v3',
-    'experiment_quandl_100assets_tft_cp12621_len252_notime_div_rsi_kd_cat_v4' 
+    'experiment_quandl_100assets_tft_cp12621_len252_notime_div_rsi_kd_cat_v4'
+    #'experiment_crypto_100assets_long_only',
+    #'experiment_crypto_100assets_tsmom',
+    #'experiment_crypto_100assets_tft_cpnone_len365_notime_div_crypto_v1',
+    #'experiment_crypto_100assets_tft_cp18330_len365_notime_div_crypto_v2',
+    #'experiment_crypto_100assets_tft_cpnone_len365_notime_div_rsi_kd_volume_crypto_v4',
+    #'experiment_crypto_100assets_tft_cp18330_len365_notime_div_rsi_kd_volume_crypto_v3',
+    #'experiment_crypto_100assets_tft_cpnone_len365_notime_div_rsi_kd_volume_cat_crypto_v3',
+    #'experiment_crypto_100assets_tft_cp18330_len365_notime_div_rsi_kd_volume_cat_crypto_v2',
 ]
 
 LEGENDS = [
@@ -46,7 +54,7 @@ for experiment in EXPERIMENTS:
         all_years_df.append(srs)
 
     combined_srs = pd.concat(all_years_df, axis=0)
-    fund_balance = (1 + combined_srs).cumprod() * 100
+    fund_balance = (1 + combined_srs).cumprod() * STARTING_BALANCE
     fund_balance.plot()
 
 plt.title('Fund Balance Over Time')
